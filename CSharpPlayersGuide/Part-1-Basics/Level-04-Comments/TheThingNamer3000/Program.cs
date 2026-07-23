954db33f248550ac994dd5b9c8b0f87c78846380
Roberto Keppler <rkeppler42@gmail.com>
2026-07-23 00:00:18 -0300

    feat: add solution for the The Thing Namer 3000 challenge

---
+++ CSharpPlayersGuide/Part-1-Basics/Level-04-Comments/TheThingNamer3000/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); // thing that we are talking about
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine(); /* description of the thing */
+string c = "of Doom"; // where it came from
+string d = "3000"; // the version of the thing
+Console.WriteLine("The " + b + " " + a + " " + c + " " + d + "!");
+
+// we could make the name of the variables more descriptive
